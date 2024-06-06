3b15c67b41559736383d7b190138af1d55283324
Derek Hamilton <125328155+dhamiltonstl@users.noreply.github.com>
2024-06-06 09:33:14 -0500

    user sign up

---
+++ SpaDay6/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace SpaDay6.Controllers;
+
+
+public class UserController : Controller
+{
+   [HttpGet("/user/add")]
+   public IActionResult Add()
+   {
+      return View();
+   }
+
+[HttpPost("/user")]
+   public IActionResult SubmitAddUserForm(User newUser, string verify) 
+   {
+   // add form submission handling code here
+      if (verify == newUser.Password)
+      {
+         ViewBag.userName = newUser.Username;
+         return View("Index");
+      }
+      ViewBag.Error = "Passwords don't match!";
+      return View("Add");
+   }
+}
